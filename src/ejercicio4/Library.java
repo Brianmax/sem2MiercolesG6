33e08ae677822b42acf60891bacda0165bb648cc
Brianmax <george.maxi@ucsp.edu.pe>
2024-07-10 22:36:24 -0500

    prestamo items

--- src/ejercicio4/Library.java
+++ src/ejercicio4/Library.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 
 public class Library {
     private ArrayList<LibraryItem> libraryItems;
-    private ArrayList<LibraryUser> libraryUsers;
+    private ArrayList<LibraryUser> users;
 
-    Library(ArrayList<LibraryItem> libraryItems, ArrayList<LibraryUser> libraryUsers) {
+    Library(ArrayList<LibraryItem> libraryItems, ArrayList<LibraryUser> users) {
         this.libraryItems = libraryItems;
-        this.libraryUsers = libraryUsers;
+        this.users = users;
     }
 
     public void addItem(LibraryItem libraryItem) {
@@ -41,9 +41,9 @@ public class Library {
     {
         boolean isFound = false;
 
-        for (int i = 0; i < libraryUsers.size(); i++)
+        for (int i = 0; i < users.size(); i++)
         {
-            LibraryUser libraryUserArray = libraryUsers.get(i);
+            LibraryUser libraryUserArray = users.get(i);
             if (libraryUserArray.getUserId() == libraryUser.getUserId()
                 || libraryUserArray.getUsername().equals(libraryUser.getUsername()))
             {
@@ -56,8 +56,62 @@ public class Library {
             System.out.println("Error en los datos del usuario");
         }
         else {
-            libraryUsers.add(libraryUser);
+            users.add(libraryUser);
             System.out.println("Usuario registrado correctamente");
         }
+    }
+    public void loanItem(int itemId, String username)
+    {
+        // implementacion
+        LibraryItem libraryItem = findItemById(itemId);
+        LibraryUser libraryUser = findByUsername(username);
+        if (libraryUser == null || libraryItem == null)
+        {
+            int a = 10;
+            System.out.println("Usuario o item no existen");
+            System.out.println(a);
+            return;
+        }
+        if (libraryItem.isLoaned())
+        {
+            System.out.println("El item no esta disponible");
+            return;
+        }
+        libraryUser.getItems().add(libraryItem);
+        libraryItem.setLoaned(true);
+    }
+    public void returnItem(int itemId, String username)
+    {
+        // implementacion
+    }
+
+    private LibraryItem findItemById(int itemId)
+    {
+//        for (int i = 0; i < libraryItems.size(); i++)
+//        {
+//            // implementacion
+//            LibraryItem libraryItemArray = libraryItems.get(i);
+//        }
+
+        for (LibraryItem item: libraryItems)
+        {
+            if (item.getItemId() == itemId)
+            {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    private LibraryUser findByUsername(String username)
+    {
+        for (LibraryUser libraryUser: users)
+        {
+            if (libraryUser.getUsername().equals(username))
+            {
+                return libraryUser;
+            }
+        }
+        return null;
     }
 }
--- src/ejercicio4/Main.java
+++ src/ejercicio4/Main.java
@@ -7,28 +7,77 @@ public class Main {
         Book book1 = new Book(
                 "Abril Rojo",
                 1,
-                true,
+                false,
                 "Santiago Roncagliolo");
-
-        book1.showDetails();
 
         LibraryItem book2 = new Book(
                 "Harry Potter",
                 2,
-                true,
+                false,
                 "J.K Rowling");
         LibraryItem dvd1 = new DVD(
                 "Spiderman",
                 4,
-                true,
+                false,
                 "Sam Raymi",
                 120);
 
-        ArrayList<LibraryItem> items = new ArrayList<>();
-        items.add(book2);
-        items.add(dvd1);
+        LibraryItem dvd2 = new DVD(
+                "Transformers 2",
+                5,
+                false,
+                "Michael Bay",
+                120);
 
-        ArrayList<Book> bookItems = new ArrayList<>();
-        bookItems.add(book1);
+        LibraryItem book3 = new Book(
+                "No se lo digas a nadie",
+                6,
+                false,
+                "Jaime Bayli"
+        );
+
+        ArrayList<LibraryItem> libraryItems = new ArrayList<>();
+        ArrayList<LibraryUser> libraryUsers = new ArrayList<>();
+        Library library1 = new Library(
+                libraryItems,
+                libraryUsers);
+
+        library1.addItem(book1);
+        library1.addItem(book2);
+        library1.addItem(book3);
+        library1.addItem(dvd1);
+        library1.addItem(dvd2);
+
+        LibraryUser libraryUser1 = new LibraryUser(
+                "george_12",
+                1,
+                new ArrayList<LibraryItem>()
+                );
+        LibraryUser libraryUser2 = new LibraryUser(
+                "pedro_12",
+                2,
+                new ArrayList<LibraryItem>()
+        );
+        LibraryUser libraryUser3 = new LibraryUser(
+                "andrea_08",
+                3,
+                new ArrayList<LibraryItem>()
+        );
+        LibraryUser libraryUser4 = new LibraryUser(
+                "vero_12",
+                4,
+                new ArrayList<LibraryItem>()
+        );
+        LibraryUser libraryUser5 = new LibraryUser(
+                "fiore_78",
+                5,
+                new ArrayList<LibraryItem>()
+        );
+
+        library1.addUser(libraryUser1);
+        library1.addUser(libraryUser2);
+        library1.addUser(libraryUser3);
+        library1.addUser(libraryUser4);
+        library1.addUser(libraryUser5);
     }
 }
